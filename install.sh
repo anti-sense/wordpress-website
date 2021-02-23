574d3b7e43eee09f0afa202a564f725e9db2084d
Bruno Costa <brunovasquescosta@gmail.com>
2021-02-23 11:30:47 +0000

    Final steps to step up install.sh for aws

--- install.sh
+++ install.sh
@@ -26,45 +26,9 @@ sudo apt-get upgrade
 curl -fsSL test.docker.com -o get-docker.sh && sh get-docker.sh
 sudo usermod -aG docker ${USERNAME}
 
-sudo apt install nginx certbot python3-certbox-nginx
-
-
-
-####
-# replace /etc/nginx/sites-enabled/default with the following 
-```
-server {
-        listen 443 ssl;
-        server_name www.anti-sense.com anti-sense.com;
-        ssl_certificate /etc/letsencrypt/live/anti-sense.com/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/anti-sense.com/privkey.pem;
-
-        location / {
-                proxy_set_header X-Real-IP $remote_addr;
-                proxy_set_header X-Forward-Proto $scheme;
-                proxy_set_header X-Forwarded-Proto $scheme;
-                proxy_set_header X-Forwarded-Host $host;
-                proxy_pass       http://localhost:80;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade $http_upgrade;
-                proxy_set_header Connection 'upgrade';
-                proxy_set_header Host $host;
-                proxy_cache_bypass $http_upgrade;
-        }
-}
-````
-
-
-sudo certbot --nginx certonly -d anti-sense.com -d www.anti-sense.com
-
-
 #MYSQL 
 sudo apt install mysql-server
 sudo mysql_secure_installation
-
-echo -n Root Password: 
-read -s root_password
-
 
 #SET GLOBAL validate_password_policy=low
 sudo mysql -e "CREATE DATABASE ${DB_NAME};"
@@ -84,55 +48,27 @@ sudo mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
 ####
 
 
+#Build website
+git clone https://github.com/anti-sense/wordpress-website.git
+cd wordpress-website
+
 #Set website
 docker build -t antisense/website .
-docker run -d --network="host"  antisense/website
-
+docker run -d --network="host" --mount source=wp-app,target=/var/www/html antisense/website
 #docker logs antisense/website
+#docker exec -it [container] /bin/bash
+
+sudo apt install nginx certbot python3-certbox-nginx
+sudo cp nginx/antisense.conf /etc/nginx/sites-enabled/
+sudo certbot --nginx certonly -d anti-sense.com -d www.anti-sense.com
 
 
 
 
 
 
-```
-docker build -t antisense/website .
-docker run -d --network="host" antisense/website
-#docker logs antisense/website
 
 
 
 
 
-
-  wp:
-    image: wordpress:latest # https://hub.docker.com/_/wordpress/
-    ports:
-      - ${IP}:3022:80 # change ip if required
-    volumes:
-      - ./config/php.conf.ini:/usr/local/etc/php/conf.d/conf.ini
-      - ./wp-app:/var/www/html # Full wordpress project
-      #- ./plugin-name/trunk/:/var/www/html/wp-content/plugins/plugin-name # Plugin development
-      #- ./theme-name/trunk/:/var/www/html/wp-content/themes/theme-name # Theme development
-    environment:
-      WORDPRESS_DB_HOST: db
-      WORDPRESS_DB_NAME: "${DB_NAME}"
-      WORDPRESS_DB_USER: root
-      WORDPRESS_DB_PASSWORD: "${DB_ROOT_PASSWORD}"
-    depends_on:
-      - db
-    links:
-      - db
-
-
-docker run --name antisense-wordpress --network="host" \ 
--e WORDPRESS_DB_HOST=127.0.0.1:3306 \
--e WORDPRESS_DB_USER=wordpress \
--e WORDPRESS_DB_PASSWORD=password \
--e WORDPRESS_DB_NAME=wordpress wordpress \ 
--e WORDPRESS_CONFIG_EXTRA=define('WP_SITEURL','https://54.166.185.175/') 
--d wordpress
-
-
-
-docker run --mount source=wp-app,target=/var/www/html --network="host"
