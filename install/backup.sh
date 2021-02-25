954d248c81bd863354dd26a4dfa421d857a05339
Bruno Costa <brunovasquescosta@gmail.com>
2021-02-25 00:18:25 +0000

    Make some corrections

--- install/backup.sh
+++ install/backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-. DIR/../.env
+. $DIR/../.env
 
 
 _now=$(date +"%m_%d_%Y")
@@ -9,7 +9,7 @@ _file="wp-data/data_$_now.sql"
 sudo mysqldump "$DB_NAME" > /home/${USER}/wordpress-website/$_file
 
 cp /home/${USER}/wordpress-website/$_file /home/${USER}/wordpress-website/latest.sql
-#scp /home/${USER}/wordpress-website/$_file brunocosta@madreputa.no-ip.org:~/~/data/Backups/computers/antisense/wp-data/
+#scp /home/${USER}/wordpress-website/$_file backup:~/~/data/Backups/computers/antisense/wp-data/
 ##run dated backup copy & compress 
 
 #cd ~/wordpress/backup
--- install/remote.sh
+++ install/remote.sh
@@ -1,6 +1,8 @@
+ssh-keygen -f ~/.ssh/antisense-website
 #Send DB
 scp ~/data/Backups/computers/antisense/wp-data/latest.sql antisense2:~/latest.sql
 #Send Github privkey
 scp ~/.ssh/antisense-website antisense2:~/.ssh/
+cat ~/.ssh/antisense-website.pub >> ~/.ssh/authorized_keys
 
-
+ssh antisense2 'echo -e "Host backup\n\tHostname madreputa.no-ip.org\n\tPort 22\n\tUser brunocosta\n\tIdentityfile ~/.ssh/antisense-website\n\tIdentitiesOnly yes" >> ~/.ssh/config'
