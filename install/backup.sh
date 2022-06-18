72cd0782fb8ca5d248faa9cc39a2a37cca7366ca
Bruno Costa <brunocosta@ip-172-30-2-137.ec2.internal>
2022-06-18 15:14:36 +0000

    Weekly backup

--- install/backup.sh
+++ install/backup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e 
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 . $DIR/../.env
 
@@ -6,13 +8,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 _now=$(date +"%Y_%m_%d")
 _file="wp-data/data_$_now.sql.gz"
 
-sudo mysqldump "$DB_NAME" | gzip > /home/${USER}/wordpress-website/$_file
+
+sudo mysqldump -u root -p "$DB_NAME" | gzip > /home/${USER}/wordpress-website/$_file
 
 cp /home/${USER}/wordpress-website/$_file /home/${USER}/wordpress-website/wp-data/latest.sql.gz
 rsync -Pvzb --suffix=.$(date +"%Y_%m_%d") /home/${USER}/wordpress-website/$_file backup:~/data/Backups/computers/antisense/wp-data/
 ##run dated backup copy & compress 
 
-cd /home/brunocosta/wordpress/backup
+cd /home/brunocosta/wordpress-website
 git add .
 git commit -m "Weekly backup"
 git push origin master
