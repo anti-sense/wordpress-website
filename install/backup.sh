ee5110653648490a46d705d92cdff0183e5e8a77
Bruno Costa <brunocosta@ip-172-30-2-137.ec2.internal>
2022-06-21 15:31:03 +0000

    Weekly backup

--- install/backup.sh
+++ install/backup.sh
@@ -8,10 +8,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 _now=$(date +"%Y_%m_%d")
 _file="wp-data/data_$_now.sql.gz"
 
-
-sudo mysqldump "$DB_NAME" | gzip > /home/${USER}/wordpress-website/$_file
-
-cp /home/${USER}/wordpress-website/$_file /home/${USER}/wordpress-website/wp-data/latest.sql.gz
 rsync -Pvzb --suffix=.$(date +"%Y_%m_%d") /home/${USER}/wordpress-website/$_file backup:~/data/Backups/computers/antisense/wp-data/
 rsync -Pvzb /home/${USER}/wordpress-website/wp-data/latest.sql.gz backup:~/data/Backups/computers/antisense/wp-data/
 ##run dated backup copy & compress 
