aca8f6944774786486f865a30875e6d4827b7bba
Bruno Costa <netbofia@hotmail.com>
2025-02-18 12:46:10 +0000

    Update setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 echo -n Database name?: 
 read DB_NAME
-echo DB_NAME=${DB_NAME} >> ../.env
+echo "DB_NAME=${DB_NAME}" > ../.env
 
 echo -n Database user?: 
 read DB_USER
-echo DB_USER=${DB_USER} >> ../.env
+echo "DB_USER=${DB_USER}" >> ../.env
 
 echo -n Database password?: 
 read DB_PASSWORD
-echo DB_PASSWORD=${DB_PASSWORD} >> ../.env
+echo "DB_PASSWORD=${DB_PASSWORD}" >> ../.env
 
 
 ../.env
